9543afb7d02a71d4d06896ee1be467f3a776bd13
mac_n_cheese <luculrichjvr@gmail.com>
2025-06-13 22:21:00 +0200

    2nd commit

--- TechCorp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TechCorp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,36 +71,31 @@ namespace TechCorp.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [Display(Name = "Full Name")]
+            public string FullName { get; set; }
+
+            [Required]
+            public string Department { get; set; }
+
+            [Required]
+            public string Role { get; set; }
+
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Confirm Password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
-
-
+        
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
@@ -114,6 +109,10 @@ namespace TechCorp.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 AdminUser user = (AdminUser)CreateUser();
+                user.Department = Input.Department;
+                user.Role = Input.Role;
+                user.FullName = Input.FullName;
+                user.Email = Input.Email;
 
                 await _userStore.SetUserNameAsync(user!, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
--- TechCorp/Services/AuthService.cs
+++ TechCorp/Services/AuthService.cs
@@ -5,14 +5,13 @@ namespace TechCorp.Services;
 
 public class AuthService
 {
+    private static readonly string defaultEmail = "admin@techcorp.com";
+    private static readonly string defaultPassword = "P@ssword1!";
+    private static readonly string adminRole = "Admin";
     public static async Task SeedDataAsync(IServiceProvider serviceProvider)
     {
         var userManager = serviceProvider.GetRequiredService<UserManager<AdminUser>>();
         var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-        string defaultEmail = "admin@techcorp.com";
-        string defaultPassword = "P@ssword1!";
-        string adminRole = "Admin";
 
         if (!await roleManager.RoleExistsAsync(adminRole))
         {
